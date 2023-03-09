264ab09dbdc40b8f0572a6248bdaa8e1e704c75f
Patrick Cash <patrickcash@gmail.com>
2023-03-09 17:31:11 -0500

    basic template to call chat pgt with prompt and get response

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Configuration, OpenAIApi } from "openai";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(null);
+
+  function generatePrompt() {
+    return ``;
+  }
+
+  const onSubmit = async () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    setResult('');
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: generatePrompt(),
+        temperature: 0.6,
+      });
+      
+      setResult(completion.data.choices[0].text);
+    } catch (e) {
+      Alert.alert("Couldn't generate review", e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Age</Text>
+      <TextInput
+        style={styles.input}
+      />
+      <Pressable onPress={onSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Generate gift ideas</Text>
+      </Pressable>
+      {result && (
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.title}>
+            Generated Review
+          </Text>
+          <Text style={styles.result}>{result}</Text>
+        </SafeAreaView>
+      )}
     </View>
   );
 }
@@ -13,8 +56,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    fontSize: 16,
+
+    borderColor: "#353740;",
+    borderWidth: 1,
+    borderRadius: 4,
+
+    padding: 16,
+    marginTop: 6,
+    marginBottom: 12,
+  },
+  label: {
+    fontSize: 16,
+    color: "gray",
+  },
+  button: {
+    marginTop: "auto",
+    backgroundColor: "#10a37f",
+    padding: 16,
+    borderRadius: 4,
+    alignItems: "center",
+    marginVertical: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
--- README.md
+++ README.md
@@ -4,10 +4,10 @@ React Native frontend for rthe ChatGPT API
 
 ## Getting Started
 
-npm start - startes teh app
+npm run start - startes teh app
 
-npm android - starts the app in teh android empulator
+npm run android - starts the app in teh android empulator
 
-npm ios - starts the app in the ios empulator
+npm run ios - starts the app in the ios empulator
 
-npm web - starts the app in a browser
+npm run web - starts the app in a browser
